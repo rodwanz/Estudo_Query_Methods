611a70796b18f08750df8eddbb26edaa76c01a84
Rodrigo Wanzeler <wanzervai@gmail.com>
2022-12-14 09:09:28 -0300

    Iniciando estudo sobre Query Methods

---
+++ src/main/java/com/devsuperior/orderBy/entities/Book.java
@@ -0,0 +1,35 @@
+package com.devsuperior.orderBy.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_book")
+public class Book implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String publisher;
+	private LocalDate publicationDate;
+	private String author;
+
+}
+
---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Grande Sertão: Veredas', 'Nova Fronteira', '2019-11-26', 'João Guimarães Rosa');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Grande Sertão: Veredas', 'Nova Fronteira', '2019-11-26', 'João Guimarães Rosa');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Grande Sertão: Veredas', 'Companhia das Letras', '2019-11-26', 'João Guimarães Rosa');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Dom Casmurro', 'Penguin Companhia das Letras', '2019-11-26', 'Machado de Assis');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Dom Casmurro', 'Antofágica', '2019-11-26', 'Machado de Assis');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Sagarana', 'Nova Fronteira', '2019-11-26', 'João Guimarães Rosa');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Sagarana', 'Global', '2019-11-26', 'João Guimarães Rosa');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Quincas Borba', 'Penguin Companhia das Letras', '2019-11-26', 'Machado de Assis');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Quincas Borba', 'Antofágica', '2019-11-26', 'Machado de Assis');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Vidas Secas', 'Record', '2019-11-26', 'Graciliano Ramos');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Vidas Secas', 'Record', '2019-11-26', 'Graciliano Ramos');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Cem Anos de Solidão', 'Record', '2019-11-26', 'Gabriel García Márquez');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Cem Anos de Solidão', 'Record', '2019-11-26', 'Gabriel García Márquez');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Crime e Castigo', 'Todavia', '2019-11-26', 'Fiódor Dostoiévski');
+INSERT INTO tb_book(name,publisher,publication_date,author) VALUES('Crime e Castigo', 'Editora 34', '2019-11-26', 'Fiódor Dostoiévski');
